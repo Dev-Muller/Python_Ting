05d90b589d89ffe31a2e77f436e3e4b8937c8a26
muller <pedromullergl11@gmail.com>
2023-09-28 16:06:46 -0300

    feat: queue logic implemented

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,23 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.items = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.items)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.items.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if not len(self.items) == 0:
+            return self.items.pop(0)
+        else:
+            raise IndexError(
+                "A fila está vazia. Não é possível remover elementos.")
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index < len(self.items):
+            return self.items[index]
+        else:
+            raise IndexError("Índice Inválido ou Inexistente")
