b1009fd211bb115fbc83018475ae0589e1da894f
muller <pedromullergl11@gmail.com>
2023-09-28 16:21:54 -0300

    feat: file management logic implemented

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if path_file.endswith(".txt"):
+            with open(path_file, 'r') as file:
+                lines = file.read().split('\n')
+            return lines
+        else:
+            sys.stderr.write("Formato inválido")
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
