8be504b4d53aa1edda3399bb85d764ff6a69b71a
muller <pedromullergl11@gmail.com>
2023-10-02 19:38:42 -0300

    feat: implemented logic for search existent word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
+from ting_file_management.queue import Queue
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for item in range(len(instance)):
+        occurrences = []
+        file = instance.search(item)
+        for index, line in enumerate(file["linhas_do_arquivo"], start=1):
+            # ou ao inves de start=1 pode ser apenas o numero
+            if word.lower() in line.lower():
+                occurrences.append({"linha": index})
+        if len(occurrences) > 0:
+            results.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": occurrences,
+            })
+    return results
 
 
 def search_by_word(word, instance):
